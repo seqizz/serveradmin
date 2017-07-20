a9e7696be5f7a29ce917cce27a9ea4b4af50c1c9
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-07-20 11:26:35 +0200

    apps: Disable delete on admin

--- serveradmin/apps/admin.py
+++ serveradmin/apps/admin.py
@@ -13,5 +13,13 @@ class ApplicationAdmin(admin.ModelAdmin):
         'disabled',
     ]
 
+    def get_actions(self, request):
+        actions = super(ApplicationAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 admin.site.register(Application, ApplicationAdmin)
